ec3d02c9ba7d15cfab9d7655205b90a84fb926f9
alumno <alumno@10.0.2.15>
2023-05-22 11:56:04 +0200

    commit 2

--- src/ejemplo/ejemplo2.java
+++ src/ejemplo/ejemplo2.java
@@ -1,7 +1,7 @@
 package ejemplo;
 
-public class ejemplo {
-//hola
+public class ejemplo2 {
+//hola2
 	  public static void main(String[] args) {
 		  System.out.println("Hola soy Jose Miguel");
 	  }
